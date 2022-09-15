6f0e2a1877dc15f4d1f31507f552c57416c91093
cphalder <91915610+cphalder@users.noreply.github.com>
2022-09-15 16:12:33 +0100

    fixed bug with hour hand

--- app.js
+++ app.js
@@ -10,12 +10,13 @@ function setDate() {
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
   const minutes = time.getMinutes();
-  const minutesDegrees = (minutes / 60) * 360 + 90;
+  const minutesDegrees = (minutes / 60) * 360 + (seconds / 60) * 6 + 90;
   minHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
-  const hour = time.getHours;
-  const hourDegrees = (hour / 12) * 360 + 90;
+  const hour = time.getHours();
+  const hourDegrees = (hour / 12) * 360 + (minutes / 60) * 30 + 90;
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
+setDate();
